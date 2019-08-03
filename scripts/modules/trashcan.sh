c8ca945ebbd3d7097580b1151269cb3dd1c54fcd
Andrew Graham <queglay@gmail.com>
2019-08-03 23:39:20 +1000

    add arguments, default dry run. -m to move to trash.

--- scripts/modules/trashcan.sh
+++ scripts/modules/trashcan.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
 # trash everything below the current path that does not have a .protect file in the folder.  it should normally only be run from the folder such as 'job/seq/shot/cache' to trash all data below this path.
+# see opmenu and firehawk_submit.py for tools to add protect files based on a top net tree for any given hip file.
+
+argument="$1"
+
+echo ""
+ARGS=''
+
+if [[ -z $argument ]] ; then
+  echo "DRY RUN. To move files to trash, use argument -m after reviewing the exclude_list.txt and you are sure it lists everything you wish to protect from being moved to the trash."
+  echo ""
+  ARGS1='--remove-source-files'
+  ARGS2='--dry-run'
+else
+  case $argument in
+    -m|--move)
+      echo "MOVING FILES TO TRASH."
+      echo ""
+      ARGS1='--remove-source-files'
+      ARGS2=''
+      ;;
+    *)
+      raise_error "Unknown argument: ${argument}"
+      return
+      ;;
+  esac
+fi
 
 current_dir=$(pwd)
 echo "current dir $current_dir"
@@ -15,14 +41,21 @@ echo "target $target"
 
 # ensure trash exists at base dir.
 mkdir -p $base_dir/$target
-
+echo ""
+echo "Build exclude_list.txt contents with directories containing .protect files"
 find . -name .protect -print0 |
     while IFS= read -r -d '' line; do
         path=$(realpath --relative-to=. "$line")
         dirname $path
     done > exclude_list.txt
 
+path_to_list=$(realpath --relative-to=. exclude_list.txt)
+echo $path_to_list >> exclude_list.txt
+
+cat exclude_list.txt
+
 cd $base_dir
+
 # run this command from the drive root, eg /prod.
-rsync -a --remove-source-files --prune-empty-dirs --inplace --relative --exclude-from="$current_dir/exclude_list.txt" --include='*' --include='*/' $source $target --dry-run -v
+rsync -a $ARGS1 --prune-empty-dirs --inplace --relative --exclude-from="$current_dir/exclude_list.txt" --include='*' --include='*/' $source $target $ARGS2 -v
 cd $current_dir

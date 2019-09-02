ab1b97c2e438d7e559f2d87e9b26e36bd94ae6bd
Andrew Graham <queglay@gmail.com>
2019-09-02 14:24:03 +1000

    stable

--- scripts/modules/deadline_post_task.py
+++ scripts/modules/deadline_post_task.py
@@ -20,53 +20,23 @@ houpythonlib = os.path.join(hfs, 'houdini/python2.7libs')
 print 'houpythonlib:', houpythonlib
 sys.path.append(houpythonlib)
 
-# import logging
+# pythonpath = os.environ['PYTHONPATH']
+# print 'pythonpath', pythonpath
 
-# formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+sys.path.append('/usr/lib64/python2.7/site-packages')
+sys.path.append('/home/deadlineuser/.local/lib/python2.7/site-packages')
+sys.path.append('/usr/lib/python2.7/site-packages')
+# menu_path = os.path.join(os.environ['FIREHAWK_HOUDINI_TOOLS'], 'scripts/s3_sync')
+# print 'menu_path', menu_path
+# sys.path.append(menu_path)
+# module_path = os.path.join(os.environ['FIREHAWK_HOUDINI_TOOLS'], 'scripts/modules')
+# print 'module_path', module_path
+# sys.path.append(module_path)
 
-# def setup_logger(name, log_file, level=logging.INFO):
-#     """Function setup as many loggers as you want"""
-
-#     handler = logging.FileHandler(log_file)        
-#     handler.setFormatter(formatter)
-
-#     logger = logging.getLogger(name)
-#     logger.setLevel(level)
-#     logger.addHandler(handler)
-
-#     return logger
-
-# import pdg
+# import pdgcmd
 
 def __main__(plugin, task_type):
     print 'main post task'
-    # print dir(plugin)
-    # print dir(task_type)
-    # current_task = plugin.GetCurrentTask()
-    # print 'plugin.GetCurrentTask', current_task
-    # print 'dir plugin.GetCurrentTask'
-    # for item in dir(current_task):
-    #     if 'get' in str(item):
-    #         try:
-    #             to_eval = "current_task."+str(item)+"()"
-    #             print 'to_eval', to_eval
-    #             print item, eval(to_eval)
-    #         except:
-    #             print 'exception', item
-    #     else:
-    #         print item
-
-    # print 'plugin.GetCurrentTaskId', plugin.GetCurrentTaskId()
-    # print 'dir plugin.GetCurrentTaskId', dir(plugin.GetCurrentTaskId())
-    # print 'plugin.GetDataFilename', plugin.GetDataFilename()
-    # print 'dir plugin.GetDataFilename', dir(plugin.GetDataFilename())
-
-    # first file logger
-    #logger = setup_logger('first_logger', '/var/tmp/logfile_post_task.log')
-    #logger.info('POST TASK SCRIPT')
-
-    # clone upstream workitem data.  didn't work on the remote side
-
     # itemname = None
     # callbackserver = None
 
@@ -79,9 +49,7 @@ def __main__(plugin, task_type):
     # except KeyError as exception: 
     #     print "ERROR: {} must be in environment or specified via argument flag.".format(exception.message)
     #     exit(1)
-
+    # print 'get work item'
     # work_item = WorkItem(getWorkItemJsonPath(itemname))
 
     # print 'dir work_item', work_item
-
-# GetCurrentTask', 'GetCurrentTaskId', 'GetDataFilename

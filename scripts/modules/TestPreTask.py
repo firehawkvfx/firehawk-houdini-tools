604a6a5fb0e19c40d62a1424941ad2296912bc42
Andrew Graham <queglay@gmail.com>
2019-09-08 13:35:19 +1000

    cleanup

--- scripts/modules/TestPreTask.py
+++ scripts/modules/TestPreTask.py
@@ -37,7 +37,7 @@ def __main__( *args ):
     deadlinePlugin = args[0]
     job = deadlinePlugin.GetJob()
 
-    deadlinePlugin.LogInfo("In Test Pre Task!")
+    deadlinePlugin.LogInfo("In Pre Task")
 
     # Get HFS
     hfs_env = job.GetJobEnvironmentKeyValue('HFS')
@@ -48,18 +48,10 @@ def __main__( *args ):
 
     # Evaluate it locallly to this machine
     hfs_env = RepositoryUtils.CheckPathMapping(hfs_env)
-    # deadlinePlugin.LogInfo("checked path mapping")
+
     # Append Houdini bin and python2.7libs folders
     append_hlibs_to_sys(hfs_env)
-    # deadlinePlugin.LogInfo("appended libs")
 
-    # # Import PDG module (don't think there is a use for this but this should work)
-    # try:
-    #     import pdg
-    # except:
-    #     deadlinePlugin.LogWarning('Unable to import pdg\n\t {}'.format(traceback.format_exc(1)))
-
-    # deadlinePlugin.LogInfo("PDG has been loaded: {}".format(pdg.__file__))
 
     # The task index (corresponds to the task file)
     startFrame = deadlinePlugin.GetStartFrame()
@@ -69,7 +61,6 @@ def __main__( *args ):
 
     # The PDG job directory will contain the task file
     jobDir = deadlinePlugin.GetPluginInfoEntryWithDefault('PDGJobDirectory', '')
-    # deadlinePlugin.LogInfo("got job dir")
     if not jobDir:
         deadlinePlugin.FailRender('PDGJobDirectory is not specified. Unable to get task file.')
 
@@ -91,26 +82,17 @@ def __main__( *args ):
     try:
         # Load the task file's data as json dict and process properties
 
-        # deadlinePlugin.LogInfo('get json data')
         json_obj = json.loads(line)
 
         
-        # deadlinePlugin.LogInfo("Read json file: {}".format( str(taskFilePath) ) )
-
         executable = RepositoryUtils.CheckPathMapping(json_obj['executable'].replace( "\"", "" ))
         arguments = RepositoryUtils.CheckPathMapping(json_obj['arguments'])
 
         # ### Alter work item command path ###
         command = arguments
-        # deadlinePlugin.LogInfo('command:')  
-        # deadlinePlugin.LogInfo(command)
 
         if '.hip' in command.lower():
-            # if a hip file is referenced, ensure it reads from the optimised mount path.
-            # command = command.replace("__PDG_DIR__", '{}'.format(self.workingDir(False)))
-
             prod_root = os.environ["PROD_ROOT"]
-            # deadlinePlugin.LogInfo('prod_root: {}'.format(prod_root))
             prod_onsite_root = os.environ["PROD_ONSITE_ROOT"]
             prod_cloud_root = os.environ["PROD_CLOUD_ROOT"]
 
@@ -128,7 +110,6 @@ def __main__( *args ):
                 result = str(target_base_path+result[len("{base}"):])
                 return result
 
-            # # quick and dirty code for testing only.  if sesi don't implement hip overiide parm path we will need to improve this.
             command_list = command.split(' ')
             new_command_list = []
             for item in command_list:
@@ -137,12 +118,8 @@ def __main__( *args ):
                 new_command_list.append(item)
             
             command = ' '.join(new_command_list)
-            # #work_item.setCommand(command)
             deadlinePlugin.LogInfo('command updated:')  
-            # deadlinePlugin.LogInfo(command)
-
             json_obj['arguments'] = command
-            # deadlinePlugin.LogInfo('Check path mapping: {}'.format(json_obj['arguments']))
 
             arguments = RepositoryUtils.CheckPathMapping(json_obj['arguments'])
 

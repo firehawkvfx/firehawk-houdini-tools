badd28c0992390f56f5e7a184103cbc26ccc9202
Andrew Graham <queglay@gmail.com>
2019-09-08 13:19:32 +1000

    alter the path to load the hip file from optimised mounts

--- scripts/modules/TestPreTask.py
+++ scripts/modules/TestPreTask.py
@@ -100,13 +100,61 @@ def __main__( *args ):
         executable = RepositoryUtils.CheckPathMapping(json_obj['executable'].replace( "\"", "" ))
         arguments = RepositoryUtils.CheckPathMapping(json_obj['arguments'])
 
+        # ### Alter work item command path ###
+        command = arguments
+        deadlinePlugin.LogInfo('command:')  
+        deadlinePlugin.LogInfo(command)
+
+        if '.hip' in command.lower():
+            # if a hip file is referenced, ensure it reads from the optimised mount path.
+            # command = command.replace("__PDG_DIR__", '{}'.format(self.workingDir(False)))
+
+            prod_root = os.environ["PROD_ROOT"]
+            # deadlinePlugin.LogInfo('prod_root: {}'.format(prod_root))
+            prod_onsite_root = os.environ["PROD_ONSITE_ROOT"]
+            prod_cloud_root = os.environ["PROD_CLOUD_ROOT"]
+
+            def convert_path(item, target_base_path):
+                result = None
+                
+                if item.startswith(prod_root):
+                    result = "{base}"+item[len(prod_root):]
+                elif item.startswith(prod_onsite_root):
+                    result = "{base}"+item[len(prod_onsite_root):]
+                elif item.startswith(prod_cloud_root):
+                    result = "{base}"+item[len(prod_cloud_root):]
+                else:
+                    deadlinePlugin.LogInfo('no path match to convert path')
+                result = str(target_base_path+result[len("{base}"):])
+                return result
+
+            # # quick and dirty code for testing only.  if sesi don't implement hip overiide parm path we will need to improve this.
+            command_list = command.split(' ')
+            new_command_list = []
+            for item in command_list:
+                if '.hip' in item.lower():
+                    item = convert_path(item, prod_root)
+                new_command_list.append(item)
+            
+            command = ' '.join(new_command_list)
+            # #work_item.setCommand(command)
+            deadlinePlugin.LogInfo('command updated:')  
+            deadlinePlugin.LogInfo(command)
+
+            json_obj['arguments'] = command
+            deadlinePlugin.LogInfo('Check path mapping: {}'.format(json_obj['arguments']))
+
+            arguments = RepositoryUtils.CheckPathMapping(json_obj['arguments'])
+
+            with open(taskFilePath, 'w') as outfile:
+                json.dump(json_obj, outfile)
+
+            deadlinePlugin.LogInfo('dump json data to file: {}'.format(taskFilePath))
+
         deadlinePlugin.LogInfo('Task Executable: %s' % executable)
         deadlinePlugin.LogInfo('Task Arguments: %s' % arguments)
 
-        with open(taskFilePath, 'w') as outfile:
-            json.dump(json_obj, outfile)
 
-        deadlinePlugin.LogInfo('dump json data to file: {}'.format(taskFilePath))
         
     except:
         deadlinePlugin.FailRender('Unable to parse task file as json\n\t {}'.format(traceback.format_exc(1)))

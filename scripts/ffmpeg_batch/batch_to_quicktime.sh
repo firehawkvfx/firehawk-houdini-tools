cb658e3c82d836d680cdb53f1fc7cbee5912d71c
Andrew Graham <queglay@gmail.com>
2019-11-14 11:38:38 +1030

    query and list files before conversion

--- scripts/ffmpeg_batch/batch_to_quicktime.sh
+++ scripts/ffmpeg_batch/batch_to_quicktime.sh
@@ -4,9 +4,35 @@
 # not great for large numbers of files that will max out ram/cores, but will get the job done quicker than one by one, and great if you have a machine with a high core count and gobs of ram.
 # Consider this post for thread limiting https://gist.github.com/Brainiarc7/2afac8aea75f4e01d7670bc2ff1afad1
 
+pass the pattern to match as a string '*.mp4'
+
 shopt -s nullglob;
-for file in "$arg"*.{mov,mp4,MOV,MP4} ; do
-    echo "convert $file"
-    ffmpeg -i "$file" -vcodec h264 -acodec aac -pix_fmt yuv420p "mp4_${file%.*}.mp4" </dev/null > /dev/null 2>&1 &
-    # this is used to background output to shell when threading multiple files - </dev/null > /dev/null 2>&1 &
+
+args=$arg
+match=$1
+
+list () {
+    for file in "$args"$match ; do
+        echo "will convert ./$file"
+    done
+}
+
+convert () {
+    echo 'converting files in background'
+    for file in "$args"$match ; do
+        echo "convert ./$file"
+        # ffmpeg -i "$file" -vcodec h264 -acodec aac -pix_fmt yuv420p "mp4/mp4_${file%.*}.mp4" &
+        ffmpeg -i "$file" -vcodec h264 -acodec aac -pix_fmt yuv420p "mp4_${file%.*}.mp4" </dev/null > /dev/null 2>&1 &
+        # this is used to background output to shell when threading multiple files - </dev/null > /dev/null 2>&1 &
+    done
+}
+
+list
+
+echo "Do you wish to batch convert the listed files?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) convert; break;;
+        No ) exit;;
+    esac
 done
